016247e9c7f33dfc403da9c7768bd5561db46803
EiMyatNwe <73009458+eimyatnwe@users.noreply.github.com>
2026-04-07 18:03:53 +0700

    config update

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,10 +3,7 @@ module.exports = {
     transform: {
         '^.+\\.js$': 'babel-jest'
     },
-    moduleNameMapper: {
-        '^bcryptjs$': '<rootDir>/backend/node_modules/bcryptjs',
-        '^jsonwebtoken$': '<rootDir>/backend/node_modules/jsonwebtoken'
-    },
+    
     modulePathIgnorePatterns: [
         '<rootDir>/frontend/'
     ]
