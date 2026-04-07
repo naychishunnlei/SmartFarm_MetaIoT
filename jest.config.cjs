ebc59e397de72b406c4961e42e728b4c887e9279
EiMyatNwe <73009458+eimyatnwe@users.noreply.github.com>
2026-04-07 18:12:39 +0700

    fix

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,10 +1,17 @@
 module.exports = {
     testEnvironment: 'node',
+    // Ensure Jest knows where to find your tests in the subfolders
+    roots: ['<rootDir>/testscripts/'],
     transform: {
-        '^.+\\.js$': 'babel-jest'
+        // Use babel-jest to transform both .js and .cjs files
+        '^.+\\.[t|j]sx?$': 'babel-jest'
     },
-    
+    // Map module paths if you use absolute imports
+    moduleNameMapper: {
+        '^(\\.{1,2}/.*)\\.js$': '$1',
+    },
     modulePathIgnorePatterns: [
-        '<rootDir>/frontend/'
+        '<rootDir>/frontend/',
+        '<rootDir>/backend/node_modules'
     ]
 }
