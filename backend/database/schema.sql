7147fe94abee7d1b165a1061de97f812a39a1a88
naychishunnlei <naychishunnlei@gmail.com>
2026-04-09 09:52:37 +0700

    database update

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -5,7 +5,9 @@ CREATE TABLE users (
     email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT NOW(),
-    updated_at TIMESTAMP DEFAULT NOW()
+    updated_at TIMESTAMP DEFAULT NOW(),
+    has_avatar BOOLEAN DEFAULT FALSE,
+    avatar_config JSONB DEFAULT '{}'::jsonb
 );
 
 -- Farms Table
